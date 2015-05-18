cb2c0d2ba2e3d164574a7794fc3725b3fd8b29f1
matteo.fiorello <matteo.fiorello@accenture.com>
2015-05-18 18:02:27 +0200

    first commit

---
+++ opl/oplinitialdata/src/pl/orange/opl/initialdata/constants/OplInitialDataConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package pl.orange.opl.initialdata.constants;
+
+import pl.orange.opl.initialdata.constants.GeneratedOplInitialDataConstants;
+
+/**
+ * Global class for all OplInitialData constants.
+ */
+public class OplInitialDataConstants extends GeneratedOplInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "oplinitialdata";
+
+	private OplInitialDataConstants()
+	{
+		//empty
+	}
+}
